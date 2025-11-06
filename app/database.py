ef1f91b07ce264e44f8c8d6c1469fcf5e5ec3e83
nyaswabuhh <alex.navitech@gmail.com>
2025-11-06 18:07:46 +0300

    dbdb

--- app/database.py
+++ app/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-db_url="postgresql://postgres:simbapos%402019@host.docker.internal:5432/fastapi_myduka"
+#db_url="postgresql://postgres:simbapos%402019@host.docker.internal:5432/fastapi_myduka"
+db_url='postgresql://myduka_user:simbapos2019@172.17.0.1:5432/fastapi_myduka'
 engine=create_engine(db_url)
 
 session=sessionmaker(autocommit=False, autoflush=False, bind=engine)
